b033390755ab53146c0c68ef58446d5f07c687d2
deathcleaver <chaos2night@gmail.com>
2016-11-04 20:06:06 +0100

    UNSTABLE - LoadLibraryA Crash

--- project/GameProject/Loader/LibraryLoader.cpp
+++ project/GameProject/Loader/LibraryLoader.cpp
@@ -1,29 +1,27 @@
 #include "LibraryLoader.hpp"
 
-bool Lib::loadLibrary(const char * libraryName)
-{
+bool Lib::loadLibrary(const char * libraryName) {
+	
 	lib = LoadLibraryA(libraryName);
 	if ( lib )
 		return true;
 	return false;
 }
 
-bool Lib::loadLibrary(const wchar_t * libraryName)
-{
+bool Lib::loadLibrary(const wchar_t * libraryName) {
+	
 	lib = LoadLibraryW(libraryName);
 	if ( lib )
 		return true;
 	return false;
 }
 
-void * Lib::getProcAddress(const char * procName)
-{
+void * Lib::getProcAddress(const char * procName) {
 	return GetProcAddress(lib, procName);
 }
 
 
-void Lib::unloadLibrary()
-{
+void Lib::unloadLibrary() {
 	FreeLibrary(lib);
 	lib = nullptr;
 }
